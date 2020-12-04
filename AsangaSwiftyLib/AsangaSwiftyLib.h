f402fca4caf1ae185c4b0a44a1ef166c4710d33c
Asanga <asangadinesh777@gmail.com>
2020-12-03 20:33:57 -0500

    Initial project setup

---
+++ AsangaSwiftyLib/AsangaSwiftyLib.h
@@ -0,0 +1,18 @@
+//
+//  AsangaSwiftyLib.h
+//  AsangaSwiftyLib
+//
+//  Created by Asanga Dinesh on 12/3/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AsangaSwiftyLib.
+FOUNDATION_EXPORT double AsangaSwiftyLibVersionNumber;
+
+//! Project version string for AsangaSwiftyLib.
+FOUNDATION_EXPORT const unsigned char AsangaSwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsangaSwiftyLib/PublicHeader.h>
+
+
---
+++ AsangaSwiftyLibExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AsangaSwiftyLibExamples
+//
+//  Created by Asanga Dinesh on 12/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
